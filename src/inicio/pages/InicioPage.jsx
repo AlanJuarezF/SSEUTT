04226fdd8dba479c9ecc83fab23fd8c53da164c4
Alan Juarez <alanwlfx@gmail.com>
2023-03-01 19:34:25 -0600

    Cambios faltantes

--- src/inicio/pages/InicioPage.jsx
+++ src/inicio/pages/InicioPage.jsx
@@ -33,7 +33,7 @@ export default function InicioPage() {
       <section id="about">
         <div className="container">
           <div className="row section-title justify-content-center">
-            <h2>¿Por qué es importante?</h2>
+            <h2 className="section-title-heading">¿Por qué es importante?</h2>
           </div>
           <div className="row justify-content-center text-center mt-5">
             <div className="col-md-4">
@@ -62,16 +62,16 @@ export default function InicioPage() {
         </div>
       </section>
       <section id="features">
-        <div class="container">
-          <div class="row section-title justify-content-center">
-            <h2 class="section-title-heading">Información Importante</h2>
+        <div className="container">
+          <div className="row section-title justify-content-center">
+            <h2 className="section-title-heading">Información Importante</h2>
           </div>
-          <div class="row mt-5">
-            <div class="col-md-4">
-              <div class="feature-block">
-                <i class="lni lni-graduation w-100 text-center"></i>
-                <h3 class="text-center">Perfil de Egreso</h3>
-                <p class="text-justify">El perfil del egresado incluye un componente estable, es decir, la
+          <div className="row mt-5">
+            <div className="col-md-4">
+              <div className="feature-block">
+              <i className="fa-solid fa-graduation-cap w-100 text-center"></i>
+                <h3 className="text-center">Perfil de Egreso</h3>
+                <p className="text-justify">El perfil del egresado incluye un componente estable, es decir, la
                   información demográfica básica de cada individuo como
                   es, género, lugar de nacimiento, fecha de nacimiento, formación, antecedentes académicos,
                   estado civil, lugar de
@@ -79,10 +79,10 @@ export default function InicioPage() {
                   tiempo (lugar de residencia,
                   nivel de ingresos, estado civil, puestos de trabajo, otros estudios, etc.).</p>
               </div>
-              <div class="feature-block mt-5">
-                <i class="lni lni-apartment w-100 text-center"></i>
-                <h3 class="text-center">Datos de empresas</h3>
-                <p class="text-justify">Los empleadores y las organizaciones a las que representan, son una
+              <div className="feature-block mt-5">
+              <i className="fa-sharp fa-solid fa-buildings w-100 text-center"></i>
+                <h3 className="text-center">Datos de empresas</h3>
+                <p className="text-justify">Los empleadores y las organizaciones a las que representan, son una
                   parte importante al que sirven las instituciones de
                   educación superior tecnológica y, en consecuencia, la información que se deriva de estos
                   debe ser analizada y utilizada
@@ -90,14 +90,14 @@ export default function InicioPage() {
                   académica en general.</p>
               </div>
             </div>
-            <div class="col-md-4 device d-flex justify-center align-items-center">
-              <img class="survey-image" src="https://sse.delicias.tecnm.mx/image/school/survey.png" alt="" />
+            <div className="col-md-4 device d-flex justify-center align-items-center">
+              <img className="survey-image" src="https://sse.delicias.tecnm.mx/image/school/survey.png" alt="" />
             </div>
-            <div class="col-md-4">
-              <div class="feature-block">
-                <i class="lni lni-handshake w-100 text-center"></i>
-                <h3 class="text-center">Desempeño del egresado</h3>
-                <p class="text-justify">Las encuestas de egresados son útiles para recopilar datos sobre la
+            <div className="col-md-4">
+              <div className="feature-block">
+                <i className="fa-solid fa-handshake w-100 text-center"></i>
+                <h3 className="text-center">Desempeño del egresado</h3>
+                <p className="text-justify">Las encuestas de egresados son útiles para recopilar datos sobre la
                   situación laboral de los egresados más recientes,
                   con el fin de obtener indicadores del desempeño profesional. Las encuestas también están
                   diseñadas para contribuir a las
@@ -106,12 +106,12 @@ export default function InicioPage() {
                   instituciones de educación superior, así como del desempeño de los egresados en el mercado
                   laboral.</p>
               </div>
-              <div class="feature-block mt-5">
-                <div class="w-100 d-flex justify-content-center">
-                  <i class="lni lni-thumbs-up"></i> <i class="lni lni-thumbs-down"></i>
+              <div className="feature-block mt-5">
+                <div className="w-100 d-flex justify-content-center">
+                <i className="fa-solid fa-thumbs-up "></i> <i className="fa-solid fa-thumbs-down"></i>
                 </div>
-                <h3 class="text-center">Expectativas del egresado</h3>
-                <p class="text-justify">Cumplido un determinado tiempo, esperamos que nuestros egresados vuelvan
+                <h3 className="text-center">Expectativas del egresado</h3>
+                <p className="text-justify">Cumplido un determinado tiempo, esperamos que nuestros egresados vuelvan
                   a completar encuestas específicas para conocer si los conocimientos adquiridos han cumplido
                   sus expectativas en el mundo laboral, que les ayudó y que es lo que podría ayudar a las
                   futuras generaciones.</p>
@@ -121,20 +121,20 @@ export default function InicioPage() {
         </div>
       </section>
       <footer id="footer">
-        <div class="container">
-          <div class="row d-flex flex-column align-items-center">
-            <div class="social-icons text-light">
-              <a class="text-decoration-none" title="Facebook" href="https://www.facebook.com/alan.juarezfragoso" target="_blank">
-                <i class="fa-brands fa-facebook"></i>
+        <div className="container">
+          <div className="row d-flex flex-column align-items-center">
+            <div className="social-icons text-light text-center">
+              <a className="text-decoration-none" title="Facebook" href="https://www.facebook.com/alan.juarezfragoso" target="_blank">
+                <i className="fa-brands fa-facebook"></i>
               </a>
-              <a class="text-decoration-none" title="Pagina Oficial" href="" target="_blank">
-              <i class="fa-brands fa-facebook"></i>
+              <a className="text-decoration-none" title="Pagina Oficial" href="" target="_blank">
+              <i className="fa-solid fa-globe"></i>
               </a>
-              <a class="text-decoration-none" title="Facebook" href="">
-              <i class="fa-brands fa-facebook"></i>
+              <a className="text-decoration-none" title="Facebook" href="">
+              <i className="fa-brands fa-instagram"></i>
               </a>
             </div>
-            <div class="copyright text-center">
+            <div className="copyright text-center">
               <p>© 2023 Alan Jaurez</p>
             </div>
           </div>
